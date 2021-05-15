4e8a633e12ebf5dbf12f8a37edf92b1e57f54fcc
Vidhi-Jain-25 <63926086+Vidhi-Jain-25@users.noreply.github.com>
2021-05-16 02:24:08 +0530

    Update rootReducer.js

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,8 +1,8 @@
 const initialState ={
     posts:[               //This is representing the data that we're getting using axios.
-        { id: 1, title: 'First post', content: 'Hey firsty' },
-        { id: 2, title: 'Second post', content: 'Hey secondy'}, 
-        { id: 3, title: 'Third post', content: 'Hey thirdy'}
+        { id: 1, title: 'First post', content: 'Hey firsty!! quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto' },
+        { id: 2, title: 'Second post', content: 'Hey secondy!! est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla'}, 
+        { id: 3, title: 'Third post', content: 'Hey thirdy!! et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut'}
     ]
 }
 
